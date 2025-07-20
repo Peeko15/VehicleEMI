b3191eab77347059835418d21aaeea0ee19caa14
Peeko15 <peekowee@gmail.com>
2025-07-20 16:20:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+process.env.TZ = "Asia/Kolkata";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
